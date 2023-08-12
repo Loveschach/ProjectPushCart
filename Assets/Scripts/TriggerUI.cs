fd8595c24fa26524007f8be6554fdd29daab1134
Loveschach <codymalach@hotmail.com>
2023-08-11 22:44:47 -0700

    More dumb game loop, and I guess I added text mesh pro I don't know if that's a good thing

---
+++ Assets/Scripts/TriggerUI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TriggerUI : MonoBehaviour
+{
+    public TMP_Text score;
+    public TMP_Text time;
+    public float GAME_LENGTH = 180;
+    int currentScore = 0;
+    float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+        score.text = "Score: 0";
+        TriggerManager.hitFoodTrigger.AddListener( UpdateScore );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time.text = "Time: " + ( GAME_LENGTH - ( Time.time - startTime ) );
+    }
+
+    void UpdateScore() {
+        currentScore++;
+        score.text = "Score: " + currentScore;
+	}
+}
