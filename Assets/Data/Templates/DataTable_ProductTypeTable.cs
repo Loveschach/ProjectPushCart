a307ea433a759d70d0b77aeb8e716dadcdb0e32d
skinnycoder <ronnlefeuvre@gmail.com>
2023-08-20 01:35:49 -0700

    Missed file

--- Assets/Data/Templates/DataTable_ProductTypeTable.cs
+++ Assets/Data/Templates/DataTable_ProductTypeTable.cs
@@ -9,8 +9,8 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Simple Data Tables/Create Instance/ProductTypeTable")]
 public class DataTable_ProductTypeTable: DataTable
 {
-	public override Color[] NewColumnColors { get; set; } = new Color[]{new Color(1f, 1f, 1f), new Color(0.9921569f, 0.6f, 0.03137255f), new Color(0.09767064f, 0.6037736f, 0f), new Color(0.6039216f, 0f, 0.4592783f), new Color(0.3773585f, 0f, 0.364374f)};
-	public override int[] NewColumnWidths { get; set; } = {100, 100, 200, 60, 60};
+	public override Color[] NewColumnColors { get; set; } = new Color[]{new Color(1f, 1f, 1f), new Color(0.9921569f, 0.6f, 0.03137255f), new Color(0.09767064f, 0.6037736f, 0f), new Color(0.6039216f, 0f, 0.4592783f), new Color(0.3773585f, 0f, 0.364374f), new Color(0.1946957f, 0f, 0.6039216f)};
+	public override int[] NewColumnWidths { get; set; } = {100, 100, 200, 60, 60, 60};
 
 	public override void Add(string NewKey)
 	{
@@ -28,5 +28,6 @@ public class DataTableRow_ProductTypeTable: DataTableRow
 	[SerializeField] public GameObject GameObject;
 	[SerializeField] public int WidthUnits;
 	[SerializeField] public int HeightUnits;
+	[SerializeField] public int DepthUnits;
 
 }
